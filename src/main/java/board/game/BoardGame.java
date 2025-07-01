83a09e0d187e641ee898a1f28bd09a9e086c732a
Đặng Thành Đạt <ddat63483@gmail.com>
2025-07-01 09:45:33 +0700

    create project

---
+++ src/main/java/board/game/BoardGame.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package board.game;
+
+/**
+ *
+ * @author LAPTOP LE SON
+ */
+public class BoardGame {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
